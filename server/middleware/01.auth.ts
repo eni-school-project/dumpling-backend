e6ed99c5543901ff6034bf50d4c67cf6d0262b58
dzyanino <dzyanino@gmail.com>
2025-11-07 22:26:01 +0300

    Add : middleware to check if the event's user context is empty or cookie is still valid (or user is logged in)

---
+++ server/middleware/01.auth.ts
@@ -0,0 +1,24 @@
+import { defineEventHandler, getCookie, getHeader } from "h3";
+import { JWTPayload } from "jose";
+import { useVerifyToken } from "../utils/jwt";
+
+export default defineEventHandler(async (event) => {
+	const tokenFromCookie = getCookie(event, 'auth_token');
+
+	if (!tokenFromCookie) event.context.user = null;
+
+	const authHeader = getHeader(event, 'Authorization') ?? getHeader(event, 'authorization');
+
+	if (!authHeader) event.context.user = null;
+
+	else {
+		const token = authHeader.startsWith('Bearer ') ? authHeader.slice(7) : null;
+
+		if (!token) event.context.user = null;
+
+		else {
+			const payload: JWTPayload | null = await useVerifyToken(token);
+			event.context.user = payload;
+		}
+	}
+});
