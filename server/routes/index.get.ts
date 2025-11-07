44f5d04754b3d294adb42aea05575c9e0749cd46
dzyanino <dzyanino@gmail.com>
2025-11-07 22:23:14 +0300

    Edit : removed default server entry point, and replaced it with two new ones based on HTTP methods

---
+++ server/routes/index.get.ts
@@ -0,0 +1,24 @@
+import { eventHandler } from "h3"
+
+// Learn more: https://nitro.build/guide/routing
+export default eventHandler((event) => {
+  return `
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>┬┴┬┴┤(･_├┬┴┬┴</title>
+</head>
+<body>
+  <h1 align="center">一═┳︻__/(¯ . ¯٥)</h1>
+  <h2 align="center">/</h2>
+  <p align="center">_______________________________</p>
+  <h4 align="center">| what are you doing here bud |</h4>
+  <p align="center">_______________________________</p>
+  <br />
+  <br />
+  <br />
+  <h6 align="center">get out...</h6>   
+</body>
+</html>`;
+});
---
+++ server/routes/index.post.ts
@@ -0,0 +1,6 @@
+import { eventHandler } from "h3"
+
+// Learn more: https://nitro.build/guide/routing
+export default eventHandler((event) => {
+  return `┬┴┬┴┤(･_├┬┴┬┴`;
+});
