02ed2d834c714d4a6f5e674e268e20277f1795b8
dzyanino <dzyanino@gmail.com>
2025-11-07 22:24:05 +0300

    Add : `jwt` util for signing and verifying token

---
+++ server/utils/jwt.ts
@@ -0,0 +1,21 @@
+import { JWTPayload, SignJWT, jwtVerify } from "jose";
+
+const secret: Uint8Array<ArrayBuffer> = new TextEncoder().encode(process.env.NITRO_JWT_SECRET);
+
+export async function useSignToken(payload: Record<string, any>): Promise<string> {
+	return await new SignJWT(payload)
+		.setProtectedHeader({ alg: 'HS256' })
+		.setIssuedAt()
+		.setExpirationTime('1d')
+		.sign(secret);
+}
+
+export async function useVerifyToken(token: string): Promise<JWTPayload | null> {
+	try {
+		const { payload } = await jwtVerify(token, secret);
+		return payload;
+	}
+	catch (error) {
+		return null;
+	}
+}
