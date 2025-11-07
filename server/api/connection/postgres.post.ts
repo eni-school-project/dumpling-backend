9471416eaa05d2d82bb43625480108d19f3aa146
dzyanino <dzyanino@gmail.com>
2025-11-07 22:27:09 +0300

    Add : dummy `mysql` and `postgres` protected route

---
+++ server/api/connection/postgres.post.ts
@@ -0,0 +1,12 @@
+import { createError, defineEventHandler } from "h3";
+
+export default defineEventHandler((event) => {
+	if (event.context.user == null) {
+		throw createError({
+			statusCode: 401,
+			statusMessage: "Client Error",
+			message: 'Unauthorized'
+		});
+	}
+	return `connected to mysql`;
+});
