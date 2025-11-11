b6c7003014ff7e17dabf856c0b16d5c61e0d32f0
dzyanino <dzyanino@gmail.com>
2025-11-11 21:54:10 +0300

    Add : types

--- server/utils/types.ts
+++ server/utils/types.ts
@@ -1,6 +1,20 @@
-enum ConnectionType { 'mysql', 'postgres' };
+enum ConnectionType {
+	mysql = 'mysql',
+	postgres = 'postgres'
+};
+
+type ConnectionCredentials = {
+	connectionType: ConnectionType,
+	host: string,
+	port?: number,
+	user: string,
+	password: string,
+	database?: string,
+}
 
 type User = {
 	connectionType: ConnectionType,
 	loginDate: string
 }
+
+export { ConnectionType, type ConnectionCredentials, type User }
