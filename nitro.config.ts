74ad236f739861de5792288116288b28b260c3b5
dzyanino <dzyanino@gmail.com>
2025-11-16 18:13:30 +0300

    Add : new runtime config for main database
    Fix : `import.meta` undefined on `es2019` and prior version

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,12 +3,18 @@ export default defineNitroConfig({
   compatibilityDate: '2025-11-09',
   srcDir: "server",
   runtimeConfig: {
-    jwtSecret: "api_token"
+    jwtSecret: "api_token",
+    databaseCredentials: "credentials",
   },
   storage: {
     kv: {
       driver: "fs",
       base: "./data/kv"
+    }
+  },
+  esbuild: {
+    options: {
+      target: "es2020"
     }
   }
 });
